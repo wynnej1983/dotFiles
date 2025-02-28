55d764bf2a85d0ea445f11f2755280fedbaad76b
Jonathan Wynne <wynnej1983@gmail.com>
2025-02-28 18:45:11 +0900

    Update ide.sh

--- tmux/ide.sh
+++ tmux/ide.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-# tmux split-window -v -p 30
-# tmux split-window -h -p 66
-# tmux split-window -h -p 50
 
 tmx () {
+    # If no session name provided, use the basename of the current working directory
+    local session_name="${1:-$(basename "$(pwd)")}"
+    local zoom_pane="${2:-1}"  # Default to zooming pane 1 (where nvim opens)
+
     # Use -d to allow the rest of the function to run
-    tmux new-session -d -s 0
-    tmux split-window -v -p 20
+    tmux new-session -d -s "$session_name"
+    # Split vertically, making the bottom pane 25% of the total height
+    tmux split-pane -v -l 25%
+    # Split the top pane horizontally
+    tmux split-pane -h
+    # Select pane 1 (top-right) and open nvim
     tmux selectp -t 1
-    tmux send-keys "vi" C-m
-    # tmux new-window -d
-    # tmux new-window -d
-    # -d to detach any other client (which there shouldn't be,
-    # since you just created the session).
-    tmux attach-session -d -t 0
+    tmux send-keys "nvim" C-m
+    # Zoom the pane with nvim (pane 1 by default, or specified by arg)
+    tmux resize-pane -Z -t "$zoom_pane"
+    tmux attach-session -d -t "$session_name"
 }
 
-tmx
+# Call the function with all provided arguments
+tmx "$@"
